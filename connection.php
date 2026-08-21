1d695cb7e408ad6940c05c332cdc10f7f1d1b7e5
Firehush1 <hansanikalhari99@gmail.com>
2026-08-21 11:12:03 +0530

    fix Database class to correct query method and improve connection setup

--- connection.php
+++ connection.php
@@ -9,14 +9,21 @@ class Database
         if (!isset(Database::$connection)) {
             Database::$connection = new mysqli("localhost","root","1234","e-store","3306");
         }
-
         }
+
   public static function iud($q)
   {
     Database::setUpconnection();
-    Database::$connection->quert
-  }
-
+    Database::$connection->query($q)
     }
+
+    public static function search($q)
+    {
+        Database::setUpconnection();
+        $resultset = Database::$connection->query($q);
+        return $resultset;
+    }
+
+    
 }
 ?>
