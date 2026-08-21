7cdf519edc3fb68fd4867b96a31660fdbf3d8857
Firehush1 <hansanikalhari99@gmail.com>
2026-08-21 10:54:03 +0530

    fix Database class setup method and correct query function

--- connection.php
+++ connection.php
@@ -5,6 +5,18 @@ class Database
     public static $connection;
 
     public static fuction setUpconnection()
-    {}
+    {
+        if (!isset(Database::$connection)) {
+            Database::$connection = new mysqli("localhost","root","1234","e-store","3306");
+        }
+
+        }
+  public static function iud($q)
+  {
+    Database::setUpconnection();
+    Database::$connection->quert
+  }
+
+    }
 }
 ?>
