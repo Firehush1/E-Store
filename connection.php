5a864b955e84480d6c8d3375c860f6aa98b87dcf
Firehush1 <hansanikalhari99@gmail.com>
2026-08-21 11:19:00 +0530

    fix Database class setup method to correct function spelling

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ class Database
 {
     public static $connection;
 
-    public static fuction setUpconnection()
+    public static function setUpconnection()
     {
         if (!isset(Database::$connection)) {
             Database::$connection = new mysqli("localhost","root","1234","e-store","3306");
