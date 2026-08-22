fe67b72977ad1f823c5464549fc36a07b956b7e9
Firehush1 <hansanikalhari99@gmail.com>
2026-08-22 13:50:49 +0530

    refactor: restructure login functionality and restore index.php

--- main.js
+++ main.js
@@ -3,19 +3,40 @@ function signin() {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-    if (email == "") {
-        alert("please enter your email");
-    } else if (password == "") {
-        alert("plase enter your password");
-    } else {
+    iet form = new FormData();
 
-        if (email == "admin@gmail.com" && password=="admin1234") {
-            alert("login successfully!");
-            window.location.href ="home.html";
-        }else{
-            alert("Invalid email or password")
+    form.append("email",email.value);
+    form.append("password", password.value);
+    
+    let request = new XMLHttpRequest;
 
+    request.open("POST","SigninProcess.php",true);
+    request.onreadystatechange = function () {
+        if(request.status == 200&&request.readyState==4){
+            let response = request.responseText;
+
+            if(response="successfully"){
+                window.location.href  = "home.html";
+            }else{
+                alert(response);
+            }
         }
+        
     }
 
+    // if (email == "") {
+    //     alert("please enter your email");
+    // } else if (password == "") {
+    //     alert("plase enter your password");
+    // } else {
+
+    //     if (email == "admin@gmail.com" && password=="admin1234") {
+    //         alert("login successfully!");
+    //         window.location.href ="home.html";
+    //     }else{
+    //         alert("Invalid email or password")
+
+    //     }
+    // }
+request.send(form);
 }
